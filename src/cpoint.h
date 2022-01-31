000d0a1f450749f79b71cd3997e579d4b3045d01
macszl <235989@edu.p.lodz.pl>
2022-01-31 21:25:13 +0100

    added some comments

--- src/cpoint.h
+++ src/cpoint.h
@@ -7,11 +7,13 @@ using namespace std;
 
 struct CPoint
 {
+
     int x;
     int y;
 
     CPoint(int _x = 0, int _y = 0) : x(_x), y(_y) {};
 
+    //over
     CPoint &operator+=(const CPoint &delta) {
         x += delta.x;
         y += delta.y;
@@ -21,9 +23,12 @@ struct CPoint
 
 struct CRect
 {
+    //the "anchor" of our rectangle
     CPoint topleft;
+    //size stores the width and length of the rectangle
     CPoint size;
 
+    //initializing both values using an initializer list
     CRect(CPoint t1 = CPoint(), CPoint s = CPoint()) :
             topleft(t1), size(s) {};
 };
--- src/screen.cpp
+++ src/screen.cpp
@@ -9,18 +9,33 @@ static CPoint BottomRight;
 static int cury, curx;
 
 void init_screen() {
+    //typical ncurses initialization functions
+
+    //initializing the screen
     initscr();
+    //disables line buffering - you don't need to press enter to transfer the characters to a program
     cbreak();
+    //user doesn't see the typed input
     noecho();
+    //timeout serves as the update speed of the whole window system
     timeout(20);
+    //ncurses translating the enter key into something different than '\n'
     nonl();
+    //function reducing the need for cursor movements
+    //usually whenever ncurses functions refresh the windows they need to redraw the cursors
+    //this function makes it so that the refresh functions don't redraw the cursor
     leaveok(stdscr, TRUE);
+    //flushing the buffer whenever a SIGSTOP, SIGINT or quitting happens
+    //set to false
     intrflush(stdscr, FALSE);
+    //unlocking the arrow keys
     keypad(stdscr, TRUE);
+    //setting the cursor mode to invisible
     curs_set(0);
 }
 
 void done_screen() {
+    //freeing all memory related to the ncurses terminal
     endwin();
 }
 
@@ -35,10 +50,13 @@ int ngetch() {
 }
 
 void getscreenanchor(int &y, int &x) {
+    //ncurses function, gets the origin position of the window(the top left corner)
     getbegyx(stdscr, y, x);
 }
 
 void getscreensize(int &y, int &x) {
+    //ncurses function, gets the the number of rows and columns the function can take
+    //stores them in y and x
     getmaxyx(stdscr, y, x);
 }
 
